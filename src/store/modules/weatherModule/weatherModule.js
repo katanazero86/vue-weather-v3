66c7b1996e0a5782719501f3432177509f65a3f8
zero86 <katanazero86@gmail.com>
2021-08-09 17:49:04 +0900

    [feature] 로직 작성 - 날씨정보를 조회해오는 로직 작성 - 주석 작성 - vuex 스토어 생성 및 작성

--- src/store/modules/weatherModule/weatherModule.js
+++ src/store/modules/weatherModule/weatherModule.js
@@ -1,19 +1,46 @@
+const mutationTypes = {
+    CURRENT_WEATHER: 'currentWeather',
+    FORECAST_5_DAY: 'forecast5Day',
+    INIT_STATES: 'initStates',
+};
+
 export const weatherModule = {
     namespaced: true,
     state() {
         return {
             currentWeather: null,
+            forecast5Day: null,
         };
     },
-    actions: {},
+    actions: {
+        currentWeatherAction({commit}, payload) {
+            commit(mutationTypes.CURRENT_WEATHER, payload);
+        },
+        forecast5DayAction({commit}, payload) {
+            commit(mutationTypes.FORECAST_5_DAY, payload);
+        },
+        initStatesAction({commit}) {
+            commit(mutationTypes.INIT_STATES);
+        },
+    },
     mutations: {
+        currentWeather(state, payload) {
+            state.currentWeather = payload;
+        },
+        forecast5Day(state, payload) {
+            state.forecast5Day = payload;
+        },
         initStates(state) {
             state.currentWeather = null;
+            state.forecast5Day = null;
         }
     },
     getters: {
         getCurrentWeather(state) {
-            return state.currentWeather
-        }
+            return state.currentWeather;
+        },
+        get5DayForecast(state) {
+            return state.forecast5Day;
+        },
     }
 }
