996e2d68a5cfaa5a329796e1be4f38c1ff85a54b
zero86 <katanazero86@gmail.com>
2021-08-10 06:03:33 +0900

    [feature] 로직 작성 - 날씨 정보를 조회 및 결과 부분 작업 + UI 작성 - isLoading 을 위한 Spinner.vue 컴포넌트 작성

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,28 @@
 <template>
   <router-view/>
+  <transition name="fade">
+    <Spinner v-if="isLoading"/>
+  </transition>
 </template>
 
 <script lang="ts">
-    import {defineComponent} from 'vue';
+    import {defineComponent, computed} from 'vue';
+    import {useStore} from 'vuex';
+    import Spinner from '@/components/Spinner/Spinner.vue';
 
     const App = defineComponent({
         name: 'App',
+        components: {
+            Spinner,
+        },
+        setup() {
+            const store = useStore();
+            const isLoading = computed(() => store.state.isLoading);
+
+            return {
+                isLoading,
+            }
+        }
     });
 
     export default App
---
+++ src/components/Spinner/Spinner.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="spinner row align-items-center justify-contents-center">
+    <div class="spinner__circle" :class="[small && `spinner__circle--small`, large && `spinner__circle--large`]"></div>
+  </div>
+</template>
+
+<script lang="ts">
+    import {defineComponent} from 'vue'
+
+    const Spinner = defineComponent({
+        name: 'Spinner',
+        props: {
+            small: {type: Boolean, default: false},
+            large: {type: Boolean, default: false},
+        }
+    })
+    export default Spinner
+</script>
+
+<style lang="scss" scoped>
+
+  @import "../../assets/scss/common/mixins";
+
+  .spinner {
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    @include overlay-background();
+
+    &__circle {
+      border: 5px solid #f3f3f3;
+      animation: spin 1s linear infinite;
+      border-top: 5px solid #555;
+      border-radius: 50%;
+      width: 50px;
+      height: 50px;
+
+      &--small {
+        width: 35px;
+        height: 35px;
+      }
+
+      &--large {
+        width: 65px;
+        height: 65px;
+      }
+
+    }
+  }
+
+  @keyframes spin {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+
+</style>
