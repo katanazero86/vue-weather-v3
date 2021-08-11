b5abcbd46210949baa55c94dff175879f5a698d3
zero86 <katanazero86@gmail.com>
2021-08-11 14:36:15 +0900

    [feature] - heroku 배포 설정 - server.js 작성 - 버그 수정

--- src/utils/axiosUtils.ts
+++ src/utils/axiosUtils.ts
@@ -4,6 +4,7 @@ import {AxiosInstance, AxiosRequestConfig, AxiosResponse} from 'axios';
 const BASE_URL: string = import.meta.env.VITE_BASE_URL.trim() || '';
 const api: AxiosInstance = axios.create({baseURL: BASE_URL});
 
+// request interceptor
 api.interceptors.request.use((config: AxiosRequestConfig) => {
     return config;
 }, (error) => {
@@ -12,6 +13,7 @@ api.interceptors.request.use((config: AxiosRequestConfig) => {
     return Promise.reject(error);
 });
 
+// response interceptor
 api.interceptors.response.use((response: AxiosResponse) => {
     return response;
 }, (error) => {
--- src/utils/dateUtils.js
+++ src/utils/dateUtils.js
@@ -1,15 +1,30 @@
 import moment from 'moment-timezone';
-
 const KOREA_TIME_ZONE = 'Asia/Seoul';
 
+/**
+ * 해당 날짜 유틸은 moment.js 에 의존성을 가지고 있습니다.
+ */
+
+/**
+ * 유닉스 타임스탬프를 날짜 문자열 형식으로 변환
+ * @param targetUnixTimestamp
+ * @param formatString
+ * @return string
+ */
 export function convertUnixTimestampIntoDateFormatting(targetUnixTimestamp, formatString = null) {
     if (formatString && formatString.trim() !== '') {
+        // formatString = 'yyyy-mm-dd hh:mm:ss' or 'yyyy-MM-dd HH:mm:ss' or etc.
         return moment.unix(targetUnixTimestamp).tz(KOREA_TIME_ZONE).format(formatString);
     }
 
     return Intl.DateTimeFormat('ko-KR', {dateStyle: 'medium', timeStyle: 'medium', timeZone: KOREA_TIME_ZONE}).format(new Date(targetUnixTimestamp * 1000));
 }
 
+/**
+ * 유닉스 타임스탬프를 Date 객체로 변환
+ * @param targetUnixTimestamp
+ * @return {Date}
+ */
 export function convertUnixTimeStampIntoDate(targetUnixTimestamp) {
     return moment.unix(targetUnixTimestamp).tz(KOREA_TIME_ZONE).toDate();
 }
